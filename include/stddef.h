d3a735b7de9b313582790a16d7c120b356bd418b
Winston Lowe <wel97459@gmail.com>
2026-05-16 15:24:15 -0700

    Add wint_t and ptrdiff_t type definitions for PORT builds

--- include/stddef.h
+++ include/stddef.h
@@ -3,6 +3,16 @@
 
 #include <PR/ultratypes.h>
 
+#if defined(PORT) && !defined(_WINT_T_DECLARED)
+typedef __WINT_TYPE__ wint_t;
+#define _WINT_T_DECLARED
+#endif
+
+#if defined(PORT) && !defined(_PTRDIFF_T_DECLARED)
+typedef __PTRDIFF_TYPE__ ptrdiff_t;
+#define _PTRDIFF_T_DECLARED
+#endif
+
 #ifdef _MSC_VER
 /* MSVC: offsetof is a compiler intrinsic */
 #ifndef offsetof
